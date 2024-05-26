a5a68f5e7caefbdd051174949e26767de587aada
Josh Ramer <josh.ramer@icloud.com>
2024-05-26 12:31:31 -0500

    Quartz sync: May 26, 2024, 12:31 PM

--- quartz/components/LinkedInProfile.tsx
+++ quartz/components/LinkedInProfile.tsx
@@ -1,12 +1,17 @@
-import LinkedInProfileBadge from 'react-linkedin-profile-badge';
-
 export default (() => {
   function LinkedInProfile() {
     return (
-      <div>
-        <LinkedInProfileBadge profileId='josh-ramer' theme='dark' size='large' orientation='horizontal' />
+      <div 
+          className={`badge-base LI-profile-badge ${rest?.className}`}
+          data-locale={"en_US"} 
+          data-size={"medium"} 
+          data-theme={"dark"} 
+          data-type={"VERTICAL"} 
+          data-vanity={"josh-ramer"}
+          data-version={"v1"}>
+        <script src="https://platform.linkedin.com/badges/js/profile.js"></script>
       </div>
-    )
+    );
   }
 
   return LinkedInProfile
