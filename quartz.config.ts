4ce3a34ad85242fe427a5ece25c6bc6526671372
Josh Ramer <josh.ramer@icloud.com>
2024-05-26 00:10:11 -0500

    Quartz sync: May 26, 2024, 12:10 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🏰 Palace Window",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
