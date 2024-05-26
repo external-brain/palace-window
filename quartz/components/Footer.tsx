82a75372ec6f2f88cd4598192d19c83487132366
Josh Ramer <josh.ramer@icloud.com>
2024-05-26 00:43:30 -0500

    Quartz sync: May 26, 2024, 12:43 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -18,7 +18,7 @@ export default ((opts?: Options) => {
             <div class="github-profile-badge" data-user="josh-ramer"></div>
           </li>
           <li>
-            <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="josh-ramer" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/josh-ramer?trk=profile-badge">Josh R.</a></div>
+            <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="josh-ramer" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/josh-ramer?trk=profile-badge"></a></div>
           </li>
         </ul>
         <script src="https://cdn.jsdelivr.net/gh/Rapsssito/github-profile-badge@latest/src/widget.min.js"></script>
