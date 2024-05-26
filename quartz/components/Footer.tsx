a1c67511805f5f8339bc43035dbaa4f9061b8a83
Josh Ramer <josh.ramer@icloud.com>
2024-05-26 10:55:48 -0500

    Quartz sync: May 26, 2024, 10:55 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -11,7 +11,19 @@ export default ((opts?: Options) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
     return (
-      <footer class={`${displayClass ?? ""}`}></footer>
+      <footer class={`${displayClass ?? ""}`}>
+        <hr />
+        <ul>
+          <li>
+            <div id="gh" data-login="josh-ramer"></div>
+          </li>
+          <li>
+            <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="HORIZONTAL" data-vanity="josh-ramer" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/josh-ramer?trk=profile-badge">Josh R.</a></div>
+          </li>
+        </ul>
+        <script src="https://platform.linkedin.com/badges/js/profile.js"></script>
+        <script src="https://lengthylyova.pythonanywhere.com/static/gh-contrib-graph/gh.js"></script>
+      </footer>
     )
   }
 
