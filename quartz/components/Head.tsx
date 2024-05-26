fe4b9976a5c7344e168108cb56ad73cbece3edf9
Josh Ramer <josh.ramer@icloud.com>
2024-05-26 00:14:50 -0500

    Quartz sync: May 26, 2024, 12:14 AM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -19,7 +19,6 @@ export default (() => {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
         <meta property="og:width" content="1200" />
