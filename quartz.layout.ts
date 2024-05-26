e8317f4bbdec73153b50b8f121e589e7c0881b5a
Josh Ramer <josh.ramer@icloud.com>
2024-05-26 00:34:48 -0500

    Quartz sync: May 26, 2024, 12:34 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,6 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/josh-ramer",
-      "Discord Community": "https://github.com/josh-ramer/discord.gg/cRFFHYye7t",
     },
   }),
 }
